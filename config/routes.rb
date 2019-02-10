3354f6fa6577bc0147cd9678e0425f0afdbd39e7
Arek Świdrak <aswidrak@interia.pl>
2019-02-10 23:25:18 +0100

    Routing concerns

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,32 @@
 Rails.application.routes.draw do
   use_doorkeeper
 
+  concern :country_code do
+    collection do
+      get ':country_code', action: :index
+    end
+  end
+
   #If api will be more complicated or need versions I would introduce another level like api/v1/
   namespace :api do
     namespace :public do
       resources :locations, only: [:index] do
-        collection do
-          get ':country_code', action: :index
-        end
+        concerns :country_code
       end
 
       resources :target_groups, only: [:index] do
-        collection do
-          get ':country_code', action: :index
-        end
+        concerns :country_code
       end
     end
 
     post 'evaluate_target', controller: :panels
 
     resources :locations, only: [:index] do
-      collection do
-        get ':country_code', action: :index
-      end
+      concerns :country_code
     end
 
     resources :target_groups, only: [:index] do
-      collection do
-        get ':country_code', action: :index
-      end
+      concerns :country_code
     end
   end
 end
